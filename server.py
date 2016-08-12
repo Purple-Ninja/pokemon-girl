0b6cf7b6445381904a88d100c71c76fb67989272
Maxis Kao <maxiskao@yahoo-inc.com>
2016-08-12 18:44:32 +0800

    integrate with Flask

--- server.py
+++ server.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, send_file
 app = Flask(__name__)
+
+import cp
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+
+    text = u'hot chick CP: 520'
+    text_color = (255,255,255)
+    fonts_path = 'fonts/MyanmarSangamMN.ttf'
+    # shadowcolor = (255, 255, 255)
+    shadowcolor = None
+    font_size = 24
+
+    input_image = 'girl.png'
+    output_image = '.'.join(input_image.split('.')[:-1]) + '-cp.png'
+
+    img_io = cp.makeup(input_image, text, text_color, fonts_path, shadowcolor, font_size, save=False)
+
+    return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == "__main__":
     app.run()
